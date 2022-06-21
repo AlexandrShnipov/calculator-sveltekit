0880e427140c7d81eeefc2404e7666e234dd7d20
AlexandrShnipov <sshnipov@gmail.com>
2022-06-21 18:38:18 +0300

    test

--- svelte.config.js
+++ svelte.config.js
@@ -1,26 +1,21 @@
-import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-static';
+import adapter from "@sveltejs/adapter-static";
+// was "@sveltejs/adapter-auto"
 
-/** @type {import('@sveltejs/kit').Config} */
+const dev = "production" === "development";
+
+/** @type {import(""@sveltejs/kit").Config} */
 const config = {
-    // Consult https://github.com/sveltejs/svelte-preprocess
-    // for more information about preprocessors
-    preprocess: preprocess(),
-
     kit: {
-        // hydrate the <div id="svelte"> element in src/app.html
-        //target: '#svelte',
         adapter: adapter({
-            pages: 'build',
-            assets: 'build',
-            fallback: null
+            pages: "docs",
+            assets: "docs"
         }),
-        prerender: {
-            default: true,
-        },
         paths: {
-            base: process.env.NODE_ENV === "production" ? "/sveltekit-gh-pages" : "",
+            // change below to your repo name
+            base: dev ? "" : "/calculator-sveltekit",
         },
+        // hydrate the <div id="svelte"> element in src/app.html
+        target: "#svelte"
     }
 };
 
