4811ce451d8704aad588457a093577a6ff19d3e3
AlexandrShnipov <sshnipov@gmail.com>
2022-06-21 19:46:35 +0300

    test

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,26 @@
-// svelte.config.js
+
+import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
 
-export default {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+    // Consult https://github.com/sveltejs/svelte-preprocess
+    // for more information about preprocessors
+    preprocess: preprocess(),
+
     kit: {
+        // hydrate the <div id="svelte"> element in src/app.html
+        //target: '#svelte',
         adapter: adapter({
-            // default options are shown
             pages: 'build',
             assets: 'build',
-            fallback: null,
-            precompress: false
+            fallback: null
         }),
+        // prerender: {
+        //     default: true
+        // },
 
-        prerender: {
-            // This can be false if you're using a fallback (i.e. SPA mode)
-            default: true
-        }
     }
 };
+
+export default config;
