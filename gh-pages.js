2a62efd55f18faedad19669b7b44ddb2dd1ad672
AlexandrShnipov <sshnipov@gmail.com>
2022-06-21 19:38:34 +0300

    test

--- gh-pages.js
+++ gh-pages.js
@@ -6,8 +6,8 @@ publish(
         branch: 'gh-pages',
         repo: 'https://alexandrshnipov.github.io/calculator-sveltekit', // Update to point to your repository
         user: {
-            name: 'Samuele de Tomasi', // update to use your name
-            email: 'samuele@stranianelli.com' // Update to use your email
+            name: 'Alexander', // update to use your name
+            email: 'alexandershnipov@gmail.com' // Update to use your email
         },
         dotfiles: true
     },
