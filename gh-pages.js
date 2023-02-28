63daca07f06732ef3d32242518335f5c7a9d5fe3
Alexandr <sshnipov@gmail.com>
2023-02-28 15:45:01 +0100

    optimization Calculator.svelte

--- gh-pages.js
+++ gh-pages.js
@@ -4,7 +4,7 @@ publish(
     'build', // path to public directory
     {
         branch: 'gh-pages',
-        repo: 'https://alexandrshnipov.github.io/calculator-sveltekit', // Update to point to your repository
+       repo: 'https://alexandrshnipov.github.io/calculator-sveltekit', // Update to point to your repository
         user: {
             name: 'Alexander', // update to use your name
             email: 'alexandershnipov@gmail.com' // Update to use your email
